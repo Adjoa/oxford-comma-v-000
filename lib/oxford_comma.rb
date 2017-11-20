d44443699741f25c1c2eb32c90df74eeea1a05ec
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 14:27:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
   if array.size > 1
     new_last_item = array.last.split.unshift("and ")
-    
     return array.join(", ") << new_last_item
   else
-    return array.join
+    return array.join(", ")
   end
 
 end
