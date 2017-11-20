bae368a0392fa1d9723a2e9581b2fe01991500e5
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 14:46:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,12 @@
 def oxford_comma(array)
   if array.size == 1
-    return array.join(", ")
+    return array.join
   elsif array.size == 2
     return array.join (" and ")
-  elsif array.size == 3
+  elsif array.size >=3
     new_last_item = ["and ", array.last].join
     array.pop
     array << new_last_item
     return array.join(", ")
-
-  # else array.size > 1
-  #   last_item = array.last
-  #
-  #   new_last_item = array.last.split.unshift("and ")
-  #   array.join(", ") << new_last_item
-  # else
-
   end
-
 end
