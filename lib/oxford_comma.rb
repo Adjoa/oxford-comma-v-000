95a91fb9c90084e39b8c0789f4663e9bce940f45
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-20 14:47:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size == 1
     # return array.join
-    return array
+    return array[0]
   elsif array.size == 2
     return array.join (" and ")
   elsif array.size >=3
